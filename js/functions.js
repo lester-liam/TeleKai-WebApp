0a0fb39f25b8ec524c54e328a9184285af4cd2e9
Lester Liam <100615313+lester-liam@users.noreply.github.com>
2023-11-06 20:47:07 +0800

    Version 1 - All Pages Compiled

--- js/functions.js
+++ js/functions.js
@@ -29,3 +29,20 @@ function deselect(x)
     var subheading = document.getElementById("subheader-text");
     subheading.innerText = "Home";
 }
+
+function select_consultOptions(x)
+{
+    var subheading = document.getElementById("subheader-text");
+
+    switch(x) {
+        case 0:
+            subheading.innerText = "Consult a doctor online";
+            break;
+        case 1:
+            subheading.innerText = "Book a clinic visit";
+            break;
+        case 2:
+            subheading.innerText = "View your consultation history";
+            break;
+    };
+}
